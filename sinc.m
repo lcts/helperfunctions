161edb9c1f439619e221cdf3f8dde72127b68ea3
lcts <ce@lcts.de>
2014-09-12 13:04:00 +0200

    added sinc.m pulse bandwidth calculations

---
+++ sinc.m
@@ -0,0 +1,27 @@
+function [ ydata, fwhm ] = sinc(varargin)
+% calculate spectral shape of rectangular pulses, i.e. the sinc function. Real pulses aren't
+% entirely rectangular and will have less high frequency content
+%
+% USAGE
+% ydata = sinc(xdata, width)
+% [ ydata, fwhm ] = sinc(xdata, width, offset)
+%
+% xdata:   the desired freqency axis, in Hz, required
+% width:   the pulselength, in seconds, required
+% offset:  the carrier frequency of the pulse, optional, default 0
+%
+% ydata:   the pulse shape
+% fwhm:    the full width at half maximum of the pulse
+%
+VERSION = '0.9';
+
+p = inputParser;
+p.addRequired('xdata', @(x)validateattributes(x,{'numeric'},{'vector', 'real'}));
+p.addRequired('width', @(x)validateattributes(x,{'numeric'},{'scalar', 'real'}));
+p.addOptional('offset', 0, @(x)validateattributes(x,{'numeric'},{'scalar', 'real'}));
+p.FunctionName = 'sinc';
+p.parse(varargin{:});
+
+ydata =  sin(pi * (p.Results.xdata - p.Results.offset) * p.Results.width)./(pi * (p.Results.xdata - p.Results.offset));
+fwhm  = 0; % yet to be implemented
+end
