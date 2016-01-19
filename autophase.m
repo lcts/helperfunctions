cc2be4a3cfc3a11e8af1f99174f8526f75d35c83
lcts <ce@lcts.de>
2016-01-19 14:04:05 +0100

    better algorithm for finding 180° error: Check on internally normalised data, not result

--- autophase.m
+++ autophase.m
@@ -39,19 +39,17 @@ data_norm = data_norm/max(abs(data_norm));
 f = @(x)sum((imag(data_norm * exp(1i*x(1))) - x(2)).^2);
 
 % find the minimum deviation from zero 
-[ phase, deviation ] = fminsearch(f, [0 0 0]);
+[ phase, deviation ] = fminsearch(f, [0 0]);
+% rotate additional 180° if necessary
+% rotation not necessary when neither (rotate 0°) or both (rotate 360°) rot180 and
+% abs(max(integral(real))) < abs(min(integral(real))) are true
+if xor(abs(max(cumtrapz(real(data_norm * exp(1i*phase(1)))))) < abs(min(cumtrapz(real(data_norm * exp(1i*phase(1)))))), p.Results.rot180)
+	phase(1) = phase(1) + pi;
+end
 % save values and correct phases
 offset = phase(2);
 phase  = phase(1);
 data   = p.Results.data * exp(1i*phase);
-
-% rotate 180° if necessary
-% rotation not necessary when neither (rotate 0°) or both (rotate 360°) rot180 and
-% abs(max(integral(real))) < abs(min(integral(real))) are true
-if xor(abs(max(cumtrapz(real(data)))) < abs(min(cumtrapz(real(data)))), p.Results.rot180)
-  data  = data * exp(1i*pi);
-  phase = phase + pi;
-end
 
 deviation = sqrt(deviation) / (length(data)*max(abs(data)));
 if strcmp(p.Results.units,'deg'); phase = phase/pi*180; end
