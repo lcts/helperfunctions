4f2459b8dc3284454797f571b838dc5ccd4679da
lcts <ce@lcts.de>
2016-02-19 13:55:27 +0100

    added option to extrapolate into noise, fixed error when y negative,added example files

--- cpgm.m
+++ cpgm.m
@@ -1,17 +1,22 @@
-function [xout,yout] = cpgm(infile,window)
+function [xout,yout] = cpgm(infile,varargin)
 % pick peaks from CPGM dataset
 %
 % [xout,yout] = cpgm(infile)
 % [xout,yout] = cpgm(infile,window)
+% [xout,yout] = cpgm(...,'extrap',<true|false>)
 %
-% infile: Filename of CPGm dataset
+% infile: string, filename of CPGM dataset
 % window: window in ns over which to integrate the echos, optional
+% extrap: extrapolate into noise until end of data or use only detected maxima
 %
 % if window is not passed, cpgm returns peak values instead
 
-if nargin < 2
-    window = false;
-end
+p = inputParser;
+p.addRequired('infile', @(x)validateattributes(x,{'char'},{'vector'}));
+p.addOptional('window', false, @(x)validateattributes(x,{'numeric'},{'scalar','real'}));
+p.addParameter('extrap', false, @(x)validateattributes(x,{'logical'},{'scalar'}));
+p.FunctionName = 'cpgm';
+parse(p,infile,varargin{:});
 
 % load data
 [x,y] = eprload(infile);
@@ -29,55 +34,58 @@ noise = noiselvl(y);
 diffvec = diff(maxtab(:,1));
 dist = mode(diffvec);
 % and record the first two times this distance occurs
-firstfit = find(diffvec == dist,2);
+firstindex = find(diffvec == dist,2);
 
 % then we calculate the total intensity of the series, with each peak from
 % the first to the second one with proper distance
 % the series with largest intensity contains the correct peaks
 total = -Inf;
-for ii = 1:firstfit(2)
-    index = maxtab(ii,1):dist:maxtab(end,1);
-    if total < sum(y(index))
-        total = sum(y(index));
-        bestindex = index;
+if p.Results.extrap
+    lastindex = length(x);
+else
+    lastindex = maxtab(end,1);
+end
+for ii = 1:firstindex(2)
+    currentindex = maxtab(ii,1):dist:lastindex;
+    if total < sum(y(currentindex))
+        total = sum(y(currentindex));
+        index = currentindex;
     end
 end
 
 % prepare figures
 hMain = findobj('Tag',mfilename);
-if isempty(hMain)
-    hMain = figure('Tag',mfilename);
-    if ~window
-        hAxes = axes('Parent',hMain);
-        xlabel(hAxes, 'time / ns');
-        ylabel(hAxes, 'intensity / a.u.');
-    else
-        
-        hAxes(1) = axes('Parent', hMain, ...
-            'Layer', 'top', ...
-            'XAxisLocation', 'Bottom', 'YAxisLocation', 'Left', ...
-            'XColor', 'k', 'YColor', 'k');
-        xlabel(hAxes(1), 'time / ns');
-        ylabel(hAxes(1), 'intensity / a.u.');
-        
-        hAxes(2) = axes('Parent', hMain, ...
-            'Position', get(hAxes(1),'Position'), ...
-            'Layer', 'top', ...
-            'XAxisLocation', 'Top', 'YAxisLocation', 'Right', ...
-            'XTickLabel', '', ...
-            'Color', 'none', 'XColor', 'k', 'YColor', [.8 0 0]);
-        ylabel(hAxes(2), 'integrated echo intensity / a.u.');
-        linkaxes(hAxes);
-    end
+if ~isempty(hMain); close(hMain); end
+
+hMain = figure('Tag',mfilename);
+if ~p.Results.window
+    hAxes = axes('Parent',hMain);
+    xlabel(hAxes, 'time / ns');
+    ylabel(hAxes, 'intensity / a.u.');
 else
-    figure(hMain);
+    
+    hAxes(1) = axes('Parent', hMain, 'Tag', 'ax1',...
+        'Layer', 'top', ...
+        'XAxisLocation', 'Bottom', 'YAxisLocation', 'Left', ...
+        'XColor', 'k', 'YColor', 'k');
+    xlabel(hAxes(1), 'time / ns');
+    ylabel(hAxes(1), 'intensity / a.u.');
+    
+    hAxes(2) = axes('Parent', hMain, 'Tag', 'ax2', ...
+        'Position', get(hAxes(1),'Position'), ...
+        'Layer', 'top', ...
+        'XAxisLocation', 'Top', 'YAxisLocation', 'Right', ...
+        'XTickLabel', '', ...
+        'Color', 'none', 'XColor', 'k', 'YColor', [.8 0 0]);
+    ylabel(hAxes(2), 'integrated echo intensity / a.u.');
+    linkaxes(hAxes);
 end
 
 % generate new data vectors and plot result
-if ~window
+if ~p.Results.window
     % from maxima
-    xout = x(bestindex);
-    yout = y(bestindex);
+    xout = x(index);
+    yout = y(index);
     plot(x,y,xout,yout,'o');
 else
     % from integral over y(max +- window/2)
@@ -85,16 +93,23 @@ else
         'LineWidth', 0.5, 'LineStyle', '-', 'Color', [0 0 .8], ...
         'Parent', hAxes(1));
     
-    xout = x(bestindex);
-    yout = zeros(length(bestindex),1);
-    for ii = 1:length(bestindex)
-        lower = iof(x,x(bestindex(ii))-window/2);
-        upper = iof(x,x(bestindex(ii))+window/2);
+    xout = x(index);
+    yout = zeros(length(index),1);
+    for ii = 1:length(index)
+        lower = iof(x,x(index(ii)) - p.Results.window/2,'mode','smaller');
+        upper = iof(x,x(index(ii)) + p.Results.window/2,'mode','larger');
         yout(ii) = sum(y(lower:upper));
-        rectangle('Position',[x(lower),0,x(upper)-x(lower),yout(ii)],...
-            'EdgeColor','none', ...
-            'FaceColor', [.8 .0 .0], ...
-            'Parent', hAxes(2));
+        if yout(ii) > 0
+            rectangle('Position',[x(lower),0,x(upper)-x(lower),yout(ii)],...
+                'EdgeColor','none', ...
+                'FaceColor', [.8 .0 .0], ...
+                'Parent', hAxes(2));
+        elseif yout(ii) < 0
+            rectangle('Position',[x(lower),yout(ii),x(upper)-x(lower),abs(yout(ii))],...
+                'EdgeColor','none', ...
+                'FaceColor', [.8 .0 .0], ...
+                'Parent', hAxes(2));
+        end
     end
     xlim(hAxes(2),'auto');
     ylim(hAxes(2),'auto');
